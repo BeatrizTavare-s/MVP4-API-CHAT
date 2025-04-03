ae444fa5eb5890cfe8d4bdba08fa9181ea909b59
Beatriz Tavares <beatriz98t@gmail.com>
2025-04-03 12:37:43 -0300

    fix: adjust server

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,41 @@
 import express from 'express'
-// import OpenAI from "openai";
+import OpenAI from "openai";
 import 'dotenv/config'
-// import limitGlobalDaily from './limitGlobalDaily'
+import limitGlobalDaily from './limitGlobalDaily'
 const app = express()
 const port = Number(process.env.PORT) || 3000;
 
-// app.use('/chat', limitGlobalDaily); 
+app.use('/chat', limitGlobalDaily); 
 
 app.get('/', (req, res) => {
   res.send('API online 🔥');
 });
 
 
-// app.get('/chat', async (req: any, res: any) => {
-//   const openai = new OpenAI({
-//     apiKey: process.env.OPENAI_API_KEY,
-//   });
-//   const prompt = req.query.prompt;
-//   if (prompt) {
-//     try{
-//       console.log(`Processing prompt: ${prompt}`);
-//       const response = await openai.chat.completions.create({
-//         model: "gpt-4o",
-//         messages: [
-//           { role: "user", content: prompt }
-//         ],
-//         temperature: 0.7,
-//       });
-//       return res.send(response.choices[0].message.content);
-//     }catch(err){
-//       console.error(err);
-//       return res.status(500).send('An error occurred');
-//     }
-//   }
-//   return res.status(200).send('Not prompt find');
-// })
+app.get('/chat', async (req: any, res: any) => {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const prompt = req.query.prompt;
+  if (prompt) {
+    try{
+      console.log(`Processing prompt: ${prompt}`);
+      const response = await openai.chat.completions.create({
+        model: "gpt-4o",
+        messages: [
+          { role: "user", content: prompt }
+        ],
+        temperature: 0.7,
+      });
+      return res.send(response.choices[0].message.content);
+    }catch(err){
+      console.error(err);
+      return res.status(500).send('An error occurred');
+    }
+  }
+  return res.status(200).send('Not prompt find');
+})
 
-// })
 console.log(process.env.PORT)
 app.listen(port, '0.0.0.0', () => {
    console.log(process.env.PORT)
